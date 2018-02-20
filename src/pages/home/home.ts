fa1cc450e91b87eb939903282262bec9fbf1f7ed
Ana Asins <aasins@tsiberia.es>
2018-02-20 13:21:23 +0100

    primer commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ANIMALES } from "../../data/data.animales";
+import { Animal } from "../../interfaces/animal.interface";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  animales:Animal[] = [];
+
+  constructor() {
+    this.animales = ANIMALES.splice(0);
+  }
+
+}
